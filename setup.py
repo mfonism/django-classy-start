c2203d0073742b4256d3b9d8e7176bb97692561e
Mfon Eti-mfon <mfonetimfon@gmail.com>
2021-02-16 18:21:05 +0100

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='django-classy-start',
-    version='0.0.1',
+    version='0.0.2',
     author='Mfon Eti-mfon',
     author_email='mfon@etimfon.com',
     description=(
